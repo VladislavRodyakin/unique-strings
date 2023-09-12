b47626da317c68ab0b4fef4abdf9bcf45bc6a48c
Vlad <rodyakin.vlad@mail.ru>
2023-09-13 00:30:41 +0700

    Добавьте файлы проекта.

---
+++ unique-strings/unique-strings.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <unordered_set>
+#include<string>
+
+int main()
+{
+	int N, ans = 0;
+	std::unordered_set<std::string> vars;
+	std::string elem;
+	std::cin >> N;
+	for (int i = 0; i < N; i++) {
+		std::cin >> elem;
+		if (vars.find(elem) == vars.end()) {
+			vars.insert(elem);
+			ans++;
+		}
+	}
+	std::cout << ans;
+	return 0;
+}
